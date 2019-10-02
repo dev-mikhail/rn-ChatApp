8ec1c5638ce0e6444404489c8e2be32d8337e4c1
Mikhail <mikhail97ru@rambler.ru>
2019-10-03 02:17:10 +0800

    created firebase config file

---
+++ ChatApp/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyDos4cEP470ZtXH-74VJCIfP-A7zpgi_uk",
+  authDomain: "another-chatapp-565f3.firebaseapp.com",
+  databaseURL: "https://another-chatapp-565f3.firebaseio.com",
+  projectId: "another-chatapp-565f3",
+  storageBucket: "",
+  messagingSenderId: "798963461814",
+  appId: "1:798963461814:web:2e6e2c321b403be7774017"
+};
+firebase.initializeApp(config);
+export default firebase;
