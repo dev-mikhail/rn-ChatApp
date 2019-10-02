c785ec740bbd9cb6b53ea34f0768859e56fb6436
Mikhail <mikhail97ru@rambler.ru>
2019-10-03 05:40:46 +0800

    added starter code

--- ChatApp/src/App.js
+++ ChatApp/src/App.js
@@ -1,10 +1,15 @@
-// src/App.js
 import React, { Component } from 'react';
 
+import { createStore, applyMiddleware } from 'redux';
 import { Provider, connect } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import ChatUI from './components/ChatUI';
 import LoginUI from './components/LoginUI';
+import rootReducer from './reducers';
+import { fetchMessages, checkUserExists } from './actions';
+
 
 const loggerMiddleware = createLogger();
 
@@ -12,21 +17,25 @@ const store = createStore(
   rootReducer,
   applyMiddleware(
     thunkMiddleware,
-    loggerMiddleware
+    //loggerMiddleware
   )
 );
+
+import { Examples } from '@shoutem/ui';
 
 const LoginOrChat = connect(
   (state) => ({
     authorized: state.user.authorized
   })
-)(({ authorized }) => {
+)(({ authorized, dispatch }) => {
   if (authorized) {
     return (<ChatUI />);
   } else {
+    dispatch(checkUserExists());
     return (<LoginUI />);
   }
 });
+
 class App extends Component {
   render() {
     return (
@@ -36,3 +45,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
